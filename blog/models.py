a0bab03428f095fb5bb10e83eb960094a273faef
Mahdi1305 <mahdihajimahdi@gmail.com>
2020-02-06 07:04:18 -0800

    requirements and settings change

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to='image/')
+    image = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.title
